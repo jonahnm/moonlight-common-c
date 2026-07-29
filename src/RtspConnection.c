8591d7c8c8f268618e178fd8bec345ddcb8512ea
Your Name <you@example.com>
2026-07-29 15:50:32 -0400

    Add PyroWave RTSP negotiation from lichiheng1998 fork

--- src/RtspConnection.c
+++ src/RtspConnection.c
@@ -1060,7 +1060,25 @@ int performRtspHandshake(PSERVER_INFORMATION serverInfo) {
             goto Exit;
         }
         
-        if ((StreamConfig.supportedVideoFormats & VIDEO_FORMAT_MASK_AV1) && strstr(response.payload, "AV1/90000")) {
+        if ((StreamConfig.supportedVideoFormats & VIDEO_FORMAT_MASK_PYROWAVE) &&
+                (serverInfo->serverCodecModeSupport & SCM_MASK_PYROWAVE) &&
+                strstr(response.payload, "PYROWAVE/90000")) {
+            if ((serverInfo->serverCodecModeSupport & SCM_PYROWAVE_HIGH10_444) &&
+                    (StreamConfig.supportedVideoFormats & VIDEO_FORMAT_PYROWAVE_HIGH10_444)) {
+                NegotiatedVideoFormat = VIDEO_FORMAT_PYROWAVE_HIGH10_444;
+            }
+            else if ((serverInfo->serverCodecModeSupport & SCM_PYROWAVE_MAIN10) || (StreamConfig.supportedVideoFormats & VIDEO_FORMAT_PYROWAVE_MAIN10)) {
+                NegotiatedVideoFormat = VIDEO_FORMAT_PYROWAVE_MAIN10;
+            }
+            else if ((serverInfo->serverCodecModeSupport & SCM_PYROWAVE_HIGH8_444) &&
+                    (StreamConfig.supportedVideoFormats & VIDEO_FORMAT_PYROWAVE_HIGH8_444)) {
+                NegotiatedVideoFormat = VIDEO_FORMAT_PYROWAVE_HIGH8_444;
+            }
+            else {
+                NegotiatedVideoFormat = VIDEO_FORMAT_PYROWAVE;
+            }
+        }
+        else if ((StreamConfig.supportedVideoFormats & VIDEO_FORMAT_MASK_AV1) && strstr(response.payload, "AV1/90000")) {
             if ((serverInfo->serverCodecModeSupport & SCM_AV1_MAIN10) && (StreamConfig.supportedVideoFormats & VIDEO_FORMAT_AV1_MAIN10)) {
                 NegotiatedVideoFormat = VIDEO_FORMAT_AV1_MAIN10;
             }
